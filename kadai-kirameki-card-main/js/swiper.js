84183ee32e8ccd75f83028739705a75a19e80289
ryu-nuno <shion9bsk@gmail.com>
2022-10-02 03:13:42 +0000

    102

--- kadai-kirameki-card-main/js/swiper.js
+++ kadai-kirameki-card-main/js/swiper.js
@@ -1,4 +1,5 @@
-$(function () {
+/* globals Swiper */
+/*$(function () {
   if (window.matchMedia('(max-width: 767px)').matches) {
     new Swiper('.swiper', {
       slidesPerView: 1.5,
@@ -40,4 +41,27 @@ $(function () {
       },
     });
   }
-})
+})*/
+
+
+
+
+var swiper = new Swiper('.swiper', {
+  centeredSlides: true,
+  slidesPerView: 2.3,
+  spaceBetween: 58,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    formatFractionCurrent: function (n) {
+        return '0'  + n;
+    },
+    formatFractionTotal: function (number) {
+        return '0'  + number;
+    }
+  },
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+});
