e592e1053f31ed8044f6149cf168b990ad8dd072
ryu-nuno <shion9bsk@gmail.com>
2022-10-03 12:29:07 +0000

    complete

--- kadai-kirameki-card/js/swiper.js
+++ kadai-kirameki-card/js/swiper.js
@@ -3,75 +3,7 @@
 
 
 
-/*レスポンシブが反応しずらい*/
-/*$(function () {
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    new Swiper('.swiper', {
-      slidesPerView: 1.4,
-      spaceBetween: 36,
-      centeredSlides: true,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-        formatFractionCurrent: function (n) {
-            return '0'  + n;
-        },
-        formatFractionTotal: function (number) {
-            return '0' +number;
-        }
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  } else {
-    new Swiper('.swiper', {
-  centeredSlides: true,
-  slidesPerView: 2.3,
-  spaceBetween: 58,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-    formatFractionCurrent: function (n) {
-        return '0'  + n;
-    },
-    formatFractionTotal: function (number) {
-        return '0'  + number;
-    }
-  },
-  navigation: {
-    prevEl: '.swiper-button-prev',
-    nextEl: '.swiper-button-next',
-  },
-});
-  }
-})*/
 
-
-
-
-/*
-var swiper = new Swiper('.swiper', {
-  centeredSlides: true,
-  slidesPerView: 2.3,
-  spaceBetween: 58,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-    formatFractionCurrent: function (n) {
-        return '0'  + n;
-    },
-    formatFractionTotal: function (number) {
-        return '0'  + number;
-    }
-  },
-  navigation: {
-    prevEl: '.swiper-button-prev',
-    nextEl: '.swiper-button-next',
-  },
-});
-*/
 
 
 
@@ -81,8 +13,8 @@ var swiper = new Swiper('.swiper', {
 
 
 var swiper = new Swiper ('.swiper', {
-  slidesPerView: 1.4,
-  spaceBetween: 36,
+  slidesPerView: 1.365,
+  spaceBetween: 38,
   centeredSlides : true,
   pagination: {
     el: '.swiper-pagination',
@@ -100,7 +32,7 @@ var swiper = new Swiper ('.swiper', {
   },
   breakpoints: {
     767: {
-      slidesPerView: 2.3,
+      slidesPerView: 2.27,
       spaceBetween: 58,
       centeredSlides : true,
     }
